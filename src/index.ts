b00f4a870206639f4cfc2ca2b977f0a760927572
kimagure44 <ivan.lara.gomez.1977@gmail.com>
2021-02-12 05:43:11 +0100

    Adding comments

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,27 @@
-import { fromEvent, Observer } from 'rxjs';
+import { fromEvent, Observer } from 'rxjs'; 
 
-const keyboardCallback: Observer<any> = {
-  next: response => {
-    const source = response.target.dataset.source;
-    const updateModels = document.querySelectorAll(`[data-model="${source}"]`);
-    updateModels.forEach(item => {
-      const hasHTML = ['DIV'].includes(item.tagName);
-      item[hasHTML ? 'innerHTML' : 'value'] = response.target.value;
-    });
+const keyboardCallback: Observer<any> = { next: response => {
+  // Obtenemos el modelo que necesitamos actualizar
+  const source = response.target.dataset.source;
+  
+  // Buscamos todos los modelos que necesitamos actualizar 
+  const updateModels = document.querySelectorAll(`[data-model="${source}"]`);
+  
+  // Empezamos a recorrer cada elemento y actualizamos su contenido 
+  updateModels.forEach(item => {
+    const hasHTML = ['DIV'].includes(item.tagName);
+    item[hasHTML ? 'innerHTML' : 'value'] = response.target.value;
+    }); 
   },
   error: err => {},
-  complete: () => {}
-};
+  complete: () => {} 
+}; 
 
+// Obtenemos todos los elementos que tenga el siguiente atributo 
 const keyboardEvent = document.querySelectorAll('[data-reactive="keyup"]');
+  
+// Creamos un observable sobre los elementos anteriores
 const keyboardEvent$ = fromEvent<KeyboardEvent>(keyboardEvent, 'keyup');
-
+ 
+// Nos suscribimos, y se ejecuta cada vez que presionamos y soltamos una tecla 
 keyboardEvent$.subscribe(keyboardCallback);
